9fadb218534440fdbe38991f52ab3ba413a52272
Dwcks <corclan19@gmail.com>
2023-09-30 12:48:30 +0700

    update sidebar and add function.js

---
+++ views/function.js
@@ -0,0 +1,20 @@
+
+
+export function setCurrentCss(path) {
+    const currentCSS = "bg-indigo-700 text-white".split(" ")
+    const defaultCSS = "text-indigo-200 hover:text-white hover:bg-indigo-700".split(" ")
+    
+    let node = document.querySelectorAll("nav a")
+    
+    for (let index = 0; index < node.length; index++) {
+        let Element = node[index];
+    
+        if (Element.innerText == path) {
+            Element.classList.add(...currentCSS)
+        }else {
+            Element.classList.remove(...currentCSS)
+            Element.classList.add(...defaultCSS)
+        }
+        
+    }
+}
