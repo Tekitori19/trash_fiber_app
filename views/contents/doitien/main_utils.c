9906c85701771919fb0d062cd6500d5209b86ccc
Dwcks <corclan19@gmail.com>
2023-10-06 10:02:13 +0700

    fix js

--- views/contents/doitien/main_utils.c
+++ views/contents/doitien/main_utils.c
@@ -4,45 +4,24 @@ EMSCRIPTEN_KEEPALIVE
 void DoiTien(int *arr ,int tien_can_doi) {
     int tien = tien_can_doi;
 
-     while (tien > 0)
+    int to_tien[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int first = 0;
+
+    for (int i = 0; i < 9; i++)
     {
-        if (tien >= 500)
+        if (tien >= to_tien[i])
         {
-            arr[0] = tien/500;
-            tien %= 500;
-        } else if (tien >= 200)
-        {
-            arr[1] = tien/200;
-            tien %= 200;
-        } else if (tien >= 100)
-        {
-            arr[2] = tien/100;
-            tien %= 100;
-        } else if (tien >= 50)
-        {
-            arr[3] = tien/50;
-            tien %= 50;
-        } else if (tien >= 20)
-        {
-            arr[4] = tien/20;
-            tien %= 20;
-        } else if (tien >= 10)
-        {
-            arr[5] = tien/10;
-            tien %= 10;
-        } else if (tien >= 5)
-        {
-            arr[6] = tien/5;
-            tien %= 5;
-        } else if (tien >= 2)
-        {
-            arr[7] = tien/2;
-            tien %= 2;
-        }else
-        {
-            tien -= 1;
-            arr[8]=0;
-            arr[8]++;
+            if (first == 0 && tien == to_tien[i])
+            {
+                first+=1;
+                continue;
+            }
+            
+            arr[i] = tien/to_tien[i];
+            tien %= to_tien[i];
+            first+=1;
         }
+        
     }
+    
 }
